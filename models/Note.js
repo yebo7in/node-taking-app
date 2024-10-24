d7881f78df88414f56db8cb08acd1c35dfbceabb
yebo7in <166575644+yebo7in@users.noreply.github.com>
2024-10-24 00:49:31 -0700

    Update Note.js

--- models/Note.js
+++ models/Note.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const noteSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, required: true },
     title: { type: String, required: true },
-    content: { type: String, required: true }, // Ensure this is required
+    content: { type: String, required: true }, 
     createdAt: { type: Date, default: Date.now },
     isStarred: { type: Boolean, default: false },
     isPinned: { type: Boolean, default: false },
